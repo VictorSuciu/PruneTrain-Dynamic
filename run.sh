bd9c2f131c217904f9a5beb7d997978bb2b4f19c
Sangkug <burningmotto@gmail.com>
2019-04-17 00:23:08 -0500

    remove run.sh

--- run.sh
+++
@@ -1,3 +0,0 @@
-#python run-script.py --data-train /mnt/dataset/imagenet-data/raw-data/train --data-val /mnt/dataset/imagenet-data/raw-data/validation --dataset imagenet --model resnet50 --num-gpus 2
-#python run-script.py --data-train /mnt/dataset/imagenet-data/raw-data/train --data-val /mnt/dataset/imagenet-data/raw-data/validation --dataset imagenet --model vgg16 --num-gpus 2
-python run-script.py --data-train /mnt/dataset/imagenet-data/raw-data/train --data-val /mnt/dataset/imagenet-data/raw-data/validation --dataset cifar10 --model resnet32 --num-gpus 2
